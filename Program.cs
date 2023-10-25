76c3e4f2b10c4546cfeb20b9f2c84a0b0889fd65
BevizLaszlo <bevizlaci@gmail.com>
2023-10-25 09:29:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using BolygoApp.src;
+using System.Text;
+
+namespace BolygoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Bolygo> bolygok = new();
+            using (StreamReader sr = new(path: @"..\..\..\src\solsys.txt", encoding: Encoding.UTF8))
+            {
+                while (!sr.EndOfStream)
+                    bolygok.Add(new Bolygo(sr.ReadLine()));
+            }
+
+            Console.WriteLine("3. feladat:");
+            Console.WriteLine($"\t3.1: {bolygok.Count} bolygó van a naprendszerben");
+            Console.WriteLine($"\t3.2: a naprendszerben a bolygóknak átlagosan {bolygok.Sum(p => p.Holdak) / (double)bolygok.Count} holdja van");
+            Console.WriteLine($"\t3.3: a legnagyobb térfogatú bolígó a {bolygok.OrderBy(p => p.TerfogatArany).Last().Nev}");
+            Console.Write("\t3.4: írd be a keresett bolygó nevét: ");
+            string bolygo = Console.ReadLine();
+            bool vanBolygo = false;
+            foreach (Bolygo b in bolygok)
+            {
+                if (b.Nev.ToLower() == bolygo.ToLower())
+                {
+                    vanBolygo = true;
+                    break;
+                }
+            }
+            Console.WriteLine($"\t\t{(vanBolygo ? "van" : "sajnos nincs")} ilyen bolygó a naprendszerben");
+            Console.Write("\t3.4: Írj be egy egész számot: ");
+
+            int holdSzam = int.Parse( Console.ReadLine() );
+            string bolygokHolddal = "[";
+            foreach (Bolygo b in bolygok)
+                if (b.Holdak > holdSzam) bolygokHolddal += $"{b.Nev}, ";
+            bolygokHolddal = bolygokHolddal.Remove(bolygokHolddal.Length - 2);
+            bolygokHolddal += "]";
+
+            Console.WriteLine($"\t\ta következő bolygókna van {holdSzam}-nál/-nél több holdja:");
+            Console.WriteLine($"\t\t{bolygokHolddal}");
+        }
+    }
+}
---
+++ src/Bolygo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BolygoApp.src
+{
+    internal class Bolygo
+    {
+        public string Nev { get; set; }
+        public int Holdak { get; set; }
+        public double TerfogatArany { get; set; }
+
+        public Bolygo(string sor)
+        {
+            string[] data = sor.Split(';');
+            Nev = data[0];
+            Holdak = int.Parse(data[1]);
+            TerfogatArany = double.Parse(data[2].Replace('.', ','));
+        }
+    }
+}
